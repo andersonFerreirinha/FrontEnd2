435b18e68da34cbba06b4faecb440935fb57e688
ferreirinha <anderson_ferreirinha@hotmail.com>
2022-03-18 02:45:56 -0300

    modificando arquivos index.html e main.js

--- aula7/js/main.js
+++ aula7/js/main.js
@@ -2,6 +2,11 @@ let inputReference = document.querySelector('#valor')
 let buttonEnviarReference = document.querySelector('#submit')
 let mainReference = document.querySelector('#conteudo')
 
+
+window.onload = function(){
+    alert("Pagina carregada")
+}
+
 buttonEnviarReference.addEventListener('click', function(event){
     event.preventDefault()
 
