e33e3986fc5f890712d45ab6d700b8020ffc706d
ferreirinha <anderson_ferreirinha@hotmail.com>
2022-03-26 15:23:59 -0300

    colocando atividade de validadção e local storage

--- aula11/home.js
+++
@@ -1,8 +0,0 @@
-let nomeUsuarioReferencia = document.querySelector('b')
-let InformacoesUsuario = JSON.parse(localStorage.getItem('InformacoesUsuario'))
-
-
-nomeUsuarioReferencia.innerHTML = InformacoesUsuario.nome + " " + InformacoesUsuario.sobrenome
-
-
-
--- aula11/main.js
+++ aula11/main.js
@@ -2,6 +2,11 @@ let botaoCadastrarReferencia = document.querySelector('#botaoCadastrar')
 let controladoresReferencia = document.querySelectorAll('.controlador')
 let inputNomeReferencia = document.querySelector('#inputNome')
 let inputSobrenomeReferencia = document.querySelector('#inputSobrenome')
+let inputComentarioReferencia = document.querySelector('#inputComentario')
+let nomeUsuarioReferencia = document.querySelector('b')
+let comentarioReferencia = document.querySelector('p')
+let InformacoesUsuario = JSON.parse(localStorage.getItem('InformacoesUsuario'))
+// let ComentariosUser = JSON.parse(localStorage.getItem('Comentarios'))
  
 
 for(let controladorReferencia of controladoresReferencia){
@@ -21,27 +26,30 @@ for(let controladorReferencia of controladoresReferencia){
      }
 
  })
-
-
-
-
-
 }
 
 
-
+// let comentarios = []
 botaoCadastrarReferencia.addEventListener('click', function (event) {
 
     let usuario = {
         nome: inputNomeReferencia.value,
-        sobrenome: inputSobrenomeReferencia.value
+        sobrenome: inputSobrenomeReferencia.value,
+        postagem: inputComentarioReferencia.value
     }
+
+    // comentarios.push(usuario)
 
     event.preventDefault()
     console.log(event)
-    localStorage.setItem('InformacoesUsuario', JSON.stringify(usuario))
-    
-    location.href = 'home.html'
+    localStorage.setItem('InformacoesUsuario', JSON.stringify(usuario)) 
+    localStorage.setItem('Comentarios',JSON.stringify(comentarios))
+    nomeUsuarioReferencia.innerHTML = InformacoesUsuario.nome
+         for(let comentarioUser of comentarios){
+         comentarioReferencia.innerHTML = `
+         <div>${comentarioUser.postagem}</div>
+       `
 
-
+    // }
 })
+
